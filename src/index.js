aed2ca554aa72dd3e3b0ef3f01855877d7cced87
Kamal Nayan Tiwary <kamalnayantiwary73@gmail.com>
2025-06-15 21:56:12 +0530

    Fix: Replace BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { HashRouter } from 'react-router-dom';  // ✅ Yeh import important hai
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import { HashRouter } from 'react-router-dom';  
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
- <Provider store={store}>
-  <React.StrictMode>
-    <HashRouter basename="/Flipkart-Website"> {/* 👈 Yeh line badli */}
-      <App />
-    </HashRouter>
-  </React.StrictMode>
-</Provider>
-);
 
-reportWebVitals();
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
