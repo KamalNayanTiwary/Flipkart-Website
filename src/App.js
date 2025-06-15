a1a82f6662efa7a4bb8beb1fcb996bda1f3b59ea
Kamal Nayan Tiwary <kamalnayantiwary73@gmail.com>
2025-06-15 21:52:28 +0530

    Fix: Corrected import for ProductPage

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home, NotFound } from './Components/default';
-import { Box } from '@mui/material'
-
-//components
-import Header from './Components/Header/Header';
-import DetailView from './Components/ItemDetails/DetailView';
-import TemplateProvider from './templates/TemplateProvider';
-import ContextProvider from './context/ContextProvider';
-import Cart from './Components/Cart/Cart';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';  // ✅ Tera Home component ka path
+import ProductPage from './Components/Product'; // ✅ Example: product page
 
 function App() {
   return (
-    <TemplateProvider>
-      <ContextProvider>
-        <BrowserRouter>
-          <Header />
-          <Box style={{marginTop: 54}}>
-            <Routes>
-              <Route path= '/' element={<Home />} />
-              <Route path= '/cart' element={<Cart />} />
-              <Route path= '/product/:id' element={<DetailView />} />
-            </Routes>
-          </Box>
-        </BrowserRouter>
-      </ContextProvider>
-    </TemplateProvider>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/product" element={<ProductPage />} />
+      {/* aur routes yaha add karo */}
+    </Routes>
   );
 }
 
